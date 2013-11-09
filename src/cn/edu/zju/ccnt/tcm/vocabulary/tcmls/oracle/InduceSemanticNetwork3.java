63daa4681c226ade3f5bb988e6cb5b6a4fdda34d
于彤 <luckyyutong@gmail.com>
2013-11-09 21:27:26 +0800

    induce sn

--- src/cn/edu/zju/ccnt/tcm/vocabulary/tcmls/oracle/InduceSemanticNetwork3.java
+++ src/cn/edu/zju/ccnt/tcm/vocabulary/tcmls/oracle/InduceSemanticNetwork3.java
@@ -1,121 +1,113 @@
 package cn.edu.zju.ccnt.tcm.vocabulary.tcmls.oracle;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
-import com.cintcm.tcmls.Constants;
 import com.cintcm.tcmls.MySQLUtils;
 
 public class InduceSemanticNetwork3 {
 
-	
 	private static String sql = "select id, instances from semantic_network";
 	private static String toSql = "insert into triple_type (triple, type) values (?,?)";
 
 	public void loadAndTransform(Connection fromCon, Connection toCon)
 			throws SQLException {
-		Map<String, Set<Integer>> sn = new HashMap<String, Set<Integer>>();
 
-		TypesService tService = new TypesService(fromCon);
-		
-		Statement stmt = toCon.createStatement();
-		PreparedStatement pStmt = toCon.prepareStatement(toSql);
-		
+		try {
 
-		ResultSet rs = stmt.executeQuery(sql);
+			// FileWriter writer = new FileWriter("insert.sql");
+			PrintWriter writer = new PrintWriter(new FileWriter("insert.sql"));
+			writer.println();
+			Statement stmt = toCon.createStatement();
+			PreparedStatement pStmt = toCon.prepareStatement(toSql);
 
-		while (rs.next()) {
-			String subject = rs.getString(1);
-			String object = rs.getString(2);
-			String property = rs.getString(3);
-			Integer id = rs.getInt(4);
+			ResultSet rs = stmt.executeQuery(sql);
 
-			if ((subject != null) && (!subject.isEmpty()) && (object != null)
-					&& (!object.isEmpty())
-					&& ((object.startsWith(Constants.TCMLS)))
-					&& (property != null) && (!property.isEmpty())) {
-				String s_id = subject.substring(Constants.TCMLS.length());
-				String o_id = object.substring(Constants.TCMLS.length());
+			while (rs.next()) {
 
-				String sType = tService.getType(s_id);
-				String oType = tService.getType(o_id);
-				if ((sType != null) && (!sType.isEmpty()) && (oType != null)
-						&& (!oType.isEmpty())) {
-					System.out.println(Constants.TCMLS + s_id + "(" + sType
-							+ ") " + property + " " + Constants.TCMLS + o_id
-							+ "(" + oType + ") ");
+				Integer id = rs.getInt(1);
+				String[] instances = rs.getString(2).split("\\|");
 
-					String triple = sType + "|" + property + "|" + oType;
-
-					if (sn.containsKey(triple)) {
-						sn.get(triple).add(id);
-					} else {
-						Set<Integer> ids = new HashSet<Integer>();
-						ids.add(id);
-						sn.put(triple, ids);
+				for (String instance : instances) {
+					try {
+						System.out.println(id + " : " + instance);
+						pStmt.setString(1, instance);
+						pStmt.setInt(2, id);
+						pStmt.executeUpdate();
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					}
-
 				}
 
 			}
 
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
 
-		for (String triple : sn.keySet()) {
+	}
 
-			Set<Integer> ids = sn.get(triple);
+	public void loadAndWrite(Connection con) throws SQLException {
 
-			Integer count = ids.size();
+		try {
 
-			System.out.println(triple + ":" + count);
-			String[] a = triple.split("\\|");
+			// FileWriter writer = new FileWriter("insert.sql");
+			PrintWriter writer = new PrintWriter(new FileWriter("insert.sql"));
+			writer.println("insert into triple_type (triple, type) values");
+			Statement stmt = con.createStatement();
 
-			try {
-				pStmt.setString(1, a[0]);
-				pStmt.setString(2, a[1]);
-				pStmt.setString(3, a[2]);
-				pStmt.setInt(4, count);
-				boolean first = true;
-				String ids_string = null; 
-				for (Integer id : ids){
-				    if (first){
-				    	ids_string = String.valueOf(id);
-				    	first = false;
-				    }else{
-				    	ids_string += "|" + id;
-				    }
+			ResultSet rs = stmt.executeQuery(sql);
+			int i = 0;
+			//int c =0;
+			while (rs.next()) {				
+
+				Integer id = rs.getInt(1);
+				String[] instances = rs.getString(2).split("\\|");
+
+				for (String instance : instances) {
+					
+					if (i == 0) {
+						
+					} else if (i == 10000){
+						writer.println(";");
+						writer.println("insert into triple_type (triple, type) values");
+						i=0;
+				    } else {
+				    	writer.println(",");
+					}
+					
+					i++;
+					System.out.println(instance + ":" + id);
+					writer.print("(" + instance + "," + id + ")");
 				}
-				pStmt.setString(5, ids_string);
-				pStmt.executeUpdate();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+				//if (c++ > 100000) break;
 
+			}
+			writer.println(";");
+			writer.flush();
+			writer.close();
+
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
-		
-	
-		
-		
-		
 
 	}
 
 	public static void main(String[] args) throws InstantiationException,
 			IllegalAccessException, ClassNotFoundException, SQLException {
 
-		
 		Connection toCon = MySQLUtils.getConnection();
 		InduceSemanticNetwork3 hierarchy = new InduceSemanticNetwork3();
-		hierarchy.loadAndTransform(toCon, toCon);
+		hierarchy.loadAndWrite(toCon);
 
 	}
 
